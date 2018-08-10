e79a399777e474eb1c385bd3bf3136871309c7e2
zhangningl <zhangningle@chingsoft.com>
2018-08-10 17:53:33 +0800

    vue-router

---
+++ vuecli/src/components/news_it.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h2>{{IT_title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      IT_title: '互联网新闻'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ vuecli/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'                           //引入vue
+import Router from 'vue-router'
+import home from '@/components/home'//引入根目录下的Hello。vue组件
+import news from '@/components/news'
+import news_it from '@/components/news_it'
+import news_ent from '@/components/news_ent'
+
+
+
+Vue.use(Router)   //vue全局使用router
+
+export default new Router({
+  routes: [                     //配置路由
+    {                           //每一个链接都是一个对象
+      path: '/',               //链接对象
+      name: '首页',            //路由名称
+      component: home         //对应的组件模板
+    },{
+      path: '/news',
+      name: '新闻',
+      component: news,
+      children: [
+        {path: '/', component: news_it},
+        {path: 'news_it', component: news_it},
+        {path: 'news_ent', component: news_ent}
+      ]
+    }
+  ]
+})
